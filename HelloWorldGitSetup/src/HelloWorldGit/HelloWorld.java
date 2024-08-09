c61973e1cdbbc7df0e5e12916f3cbafd5d77c8f9
Gary <Gary@Gary-Lenovo>
2024-08-09 21:13:49 +0100

    First Commit

---
+++ HelloWorldGitSetup/src/HelloWorldGit/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package HelloWorldGit;
+
+/**
+ * @author Gary
+ *Program will be used to setup git push
+ */
+public class HelloWorld {
+
+	/**
+	 * 
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+
+	}//end of main
+
+}//end of class
